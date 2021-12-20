e5cafb6069b97104e4f9f348f17ec77307951afb
Bulut <bulutcakan@gmail.com>
2021-12-20 15:49:14 +0300

    Add files via upload

---
+++ spring-redis-as-database/src/main/java/com/bulut/redis/SpringRedisAsDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.bulut.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisAsDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisAsDatabaseApplication.class, args);
+    }
+
+}
